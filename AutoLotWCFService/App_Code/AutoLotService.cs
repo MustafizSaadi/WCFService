556ba676a93c8411d730f0a637697b7cc5b1998f
MustafizSaadi <mustafizur.rahman.1167141@gmail.com>
2023-03-12 11:27:17 +0600

    first commit

---
+++ AutoLotWCFService/App_Code/AutoLotService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using AutoLotDAL.Models;
+using AutoLotDAL.Repos;
+using AutoMapper;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service" in code, svc and config file together.
+public class AutoLotService : IAutoLotService
+{
+    private readonly IMapper mapper;
+    public AutoLotService()
+	{
+		mapper = new Mapper(new MapperConfiguration (cfg => cfg.CreateMap<Inventory, InventoryRecord>()));
+	}
+
+    public List<InventoryRecord> GetInventory()
+    {
+        var repo = new InventoryRepo();
+        var records = repo.GetAll();
+        var results = mapper.Map<List<InventoryRecord>>(records);
+        repo.Dispose();
+        return results;
+    }
+
+    public void InsertCar(string make, string color, string petname)
+    {
+        var repo = new InventoryRepo();
+        repo.Add(new Inventory { Color = color, Make = make, PetName = petname });
+        repo.Dispose();
+    }
+
+    public void InsertCar(InventoryRecord car)
+    {
+        var repo = new InventoryRepo();
+        repo.Add(new Inventory { Color = car.Color, Make = car.Make, PetName = car.PetName });
+        repo.Dispose();
+    }
+}
---
+++ AutoLotWCFService/App_Code/IAutoLotService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using AutoLotDAL.Models;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService" in both code and config file together.
+[ServiceContract]
+public interface IAutoLotService
+{
+
+	[OperationContract]
+	void InsertCar(string make, string color, string petname);
+	[OperationContract(Name = "InsertCarWithDetails")]
+	void InsertCar(InventoryRecord car);
+	[OperationContract]
+	List<InventoryRecord> GetInventory();
+
+	// TODO: Add your service operations here
+}
+
+// Use a data contract as illustrated in the sample below to add composite types to service operations.
+[DataContract]
+public class CompositeType
+{
+	bool boolValue = true;
+	string stringValue = "Hello ";
+
+	[DataMember]
+	public bool BoolValue
+	{
+		get { return boolValue; }
+		set { boolValue = value; }
+	}
+
+	[DataMember]
+	public string StringValue
+	{
+		get { return stringValue; }
+		set { stringValue = value; }
+	}
+}
